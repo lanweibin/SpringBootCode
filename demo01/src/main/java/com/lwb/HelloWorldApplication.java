f93352cb0346bf99ddd2ff7598de78856a32651d
lanweibin <lanweibin0203@outlook.com>
2019-11-20 23:41:22 +0800

    helloworld

---
+++ demo01/src/main/java/com/lwb/HelloWorldApplication.java
@@ -0,0 +1,18 @@
+package com.lwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: demo01
+ * @description:
+ * @author: LWB
+ * @create: 2019-11-17 18:27
+ **/
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
---
+++ demo01/src/main/java/com/lwb/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.lwb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @program: demo01
+ * @description:
+ * @author: LWB
+ * @create: 2019-11-17 18:28
+ **/
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
