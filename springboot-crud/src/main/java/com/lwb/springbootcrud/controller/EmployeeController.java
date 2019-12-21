dec3a9639828d4c57e282a9f5d1e68e5f2054643
lanweibin <lanweibin0203@outlook.com>
2019-12-21 19:49:53 +0800

    员工添加

--- springboot-crud/src/main/java/com/lwb/springbootcrud/controller/EmployeeController.java
+++ springboot-crud/src/main/java/com/lwb/springbootcrud/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.lwb.springbootcrud.controller;
 
+import com.lwb.springbootcrud.dao.DepartmentDao;
 import com.lwb.springbootcrud.dao.EmployeeDao;
+import com.lwb.springbootcrud.entities.Department;
 import com.lwb.springbootcrud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -22,6 +25,8 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
     @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> all = employeeDao.getAll();
@@ -30,4 +35,19 @@ public class EmployeeController {
         return "emp/list";
     }
 
+    @GetMapping("emp")
+    public String toAddPage(Model model){
+
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee ){
+
+        employeeDao.save(employee);
+        System.out.println(employee.toString());
+        return "redirect:/emps";
+    }
 }
