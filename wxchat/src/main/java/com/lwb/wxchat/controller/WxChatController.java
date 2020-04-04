10b62c8e0c9e289868145cd6b5b9963db1bd1209
lanweibin <lanweibin0203@outlook.com>
2020-04-04 15:56:37 +0800

    master

--- wxchat/src/main/java/com/lwb/wxchat/controller/WxChatController.java
+++ wxchat/src/main/java/com/lwb/wxchat/controller/WxChatController.java
@@ -1,12 +1,20 @@
 package com.lwb.wxchat.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @program: SpringBootCode
  * @description:
  * @author: LWB
  * @create: 2020-04-04 15:50
  **/
+@RestController
 public class WxChatController {
 
+    @GetMapping("/wx")
+    public void access(){
+
+    }
 
 }
